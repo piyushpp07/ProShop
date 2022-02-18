a8527df739ef37b3937d4b71ad233c302f3f4d09
piyushpp07 <piyushpar7@gmail.com>
2022-02-18 12:14:12 +0530

    18-02-2021 Setup Backend and model setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import products from './data/products.js'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
+
 const app = express()
 dotenv.config()
+
+connectDB()
 
 app.get('/', (req, res) => {
    res.send("API is running")
