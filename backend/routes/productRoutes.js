d9164939cc9a036f2219c149ffb44add33f140c0
piyushpp07 <piyushpar7@gmail.com>
2022-02-27 01:13:39 +0530

    27-02-2022

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,29 +1,34 @@
 import express from 'express'
-const router = express.Router()
 import asyncHandler from 'express-async-handler'
+const router = express.Router()
 import Product from '../models/productModel.js'
 
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({})
 
-//@desc fetch all products
-//@route GET /api/products
-//@access Public
-router.get('/', asyncHandler(async (req, res) => {
-   const products = await Product.find({})
-   res.json(products)
-}))
+    res.json(products)
+  })
+)
 
-//@desc fetch single products
-//@route GET /api/products/:id
-//@access Public
-router.get('/:id', async (req, res) => {
-   const product = await Product.findById(req.params.id)
-   if (product) {
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
       res.json(product)
-   }
-   else {
-      res.status(404)
-      throw new Error("Product not Found")
-   }
-})
+    } else {
+      res.status(404).json({ message: 'Product not found' })
+    }
+  })
+)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,13 @@ app.use(errorHandler)
 app.get('/', (req, res) => {
    res.send("API is running")
 })
-app.get('/api/products', (req, res) => { res.json(products) })
+
 app.get('/api/products/:id', (req, res) => {
    const product = products.find(p => p._id === req.params.id)
    res.json(product)
 })
+app.use('/api/products', productRoutes)
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT,
    console.log(`App running in ${process.env.NODE_ENV} mode on port  ${PORT}`))
